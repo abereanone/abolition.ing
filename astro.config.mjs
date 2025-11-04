ff3dea10d4bc6ba3e7031809d6348b2a8ebbebb2
Michael A. Coughlin <abereanone@pm.me>
2025-11-04 08:56:14 -0500

    added 404 file build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from "astro/config";
 import path from "node:path";
 
 export default defineConfig({
+  build: {
+    format: "file",
+  },
   vite: {
     resolve: {
       alias: {
